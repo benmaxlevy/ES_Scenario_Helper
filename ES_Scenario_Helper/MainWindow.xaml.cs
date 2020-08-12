dc541c7cdd2965793ef92e9448bdb1a61d7068fa
unknown <general@simplexwebsites.com>
2020-08-11 23:56:38 -0400

    Generating a/c positions work. Next, do flight plans.

--- ES_Scenario_Helper/MainWindow.xaml.cs
+++ ES_Scenario_Helper/MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace ES_Scenario_Helper
         {
             InitializeComponent();
         }
+        
+        private String GenerateAcPosition(String Lat, String Lon, String Alt, String GndSpd, String Hdg, Random rand)
+        {
+            //array for a bunch of callsigns
+            string[] callsigns = //13 (from the 0th index)
+            {
+                "AAL",
+                "UAL",
+                "HAL", 
+                "NKS",
+                "FFT",
+                "DAL",
+                "UPS",
+                "FDX",
+                "JBU",
+                "EXJ",
+                "ASA",
+                "AAY",
+                "DLH",
+                "BAW"
+            };
+            //rand num for selecting callsign
+            int callsign = rand.Next(0, 13);
+
+            //rand num for callsign num
+            int callsignNum = rand.Next(1, 9999);
+
+            //rand num for squawk
+            int squawk = rand.Next(1000, 7000);
+
+            String Position = "@N:" + callsigns[callsign] + callsignNum + ":" + squawk + ":1:";
+
+            Position = Position + Lat + ":" + Lon + ":" + Alt + ":" + GndSpd + ":0:" + Hdg + ":0";
+
+            return Position;
+        }
+
+        private void Generate(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "";
+
+            Random rand = new Random();
+
+            for (int i = 0; i < Int16.Parse(NumAc.Text); i++)
+            {
+                Result.Text += this.GenerateAcPosition(Lat.Text, Lon.Text, Alt.Text, GndSpd.Text, Hdg.Text, rand) + "\n";
+            }
+        }
     }
 }
